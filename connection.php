4818603b51891f519491f77de50449f8171ab596
DK2121207 <davinkwan2@gmail.com>
2024-10-15 14:14:42 +0700

    adjust connect.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 // Konfigurasi database
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "db_user";
+$servername = "db-php-app-2.cruftcytajjt.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "phpapp2Admin";
+$dbname = "db-php-2";
 
 // Membuat koneksi ke MySQL
 $conn = mysqli_connect($servername, $username, $password, $dbname);
